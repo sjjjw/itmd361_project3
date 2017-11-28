2ef05ca1bc73b04e826ccc76fb267656d3c1cc47
sjjjw <31462488+sjjjw@users.noreply.github.com>
2017-11-27 19:53:54 -0600

    add the description about markers

--- js/map.js
+++ js/map.js
@@ -1,16 +1,34 @@
 function initMap() {
+    var location = {lat: 37.375177, lng: 126.632846};
     var location1 = {lat: 37.557189, lng: 126.992194};
-    var location2 = {lat: 37.375177, lng: 126.632846};
+    
+    var desc = 'My university called Incheon National Universiy is located in Songdo, Incheon';
+    var desc1 = 'Capital of Korea: Seoul';   
+    var info = new google.maps.InfoWindow({
+      content: desc
+    });
+    var info1 = new google.maps.InfoWindow({
+      content: desc1
+    });
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 5,
-      center: location1
+      zoom: 9,
+      center: location
+    });
+    
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
     });
     var marker1 = new google.maps.Marker({
       position: location1,
       map: map
     });
-    var marker2 = new google.maps.Marker({
-      position: location2,
-      map: map
+           
+    google.maps.event.addListener(marker, 'click', function(){
+      info.open(map, marker);
+      info1.open(map, marker1);
+    });
+    google.maps.event.addListener(marker1, 'click', function(){
+      info1.open(map, marker1);
     });
 }
